d9a844ef468dde77cb3e20e6bd8c1a6b394ba3b3
andre <andreantoine0016@gmail.com>
2020-08-08 09:26:20 +0200

    Initial Commit

---
+++ pARRis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pARRis
+//
+//  Created by Andre Antoine Saint Ange on 08/08/2020.
+//  Copyright © 2020 Andre Antoine Saint Ange. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
